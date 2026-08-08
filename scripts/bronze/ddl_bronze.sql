93de1333200c4b182e4dc509bb88f3a7e14aea79
purnimasrivastava2308 <purnimasrivastava2308@gmail.com>
2026-08-09 02:17:14 +0530

    Refactor table schemas in ddl_bronze.sql
    Updated data types for several columns in customer, product, sales, and ERP tables.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -101,15 +101,13 @@ USE RetailAnalyticsDW_bronze;
 DROP TABLE IF EXISTS crm_cust_info;
 
 CREATE TABLE crm_cust_info (
-
-    cst_id INT,
-    cst_key VARCHAR(20),
-    cst_firstname VARCHAR(50),
-    cst_lastname VARCHAR(50),
-    cst_marital_status CHAR(1),
-    cst_gndr CHAR(1),
-    cst_create_date DATE
-
+    cst_id             INT,
+    cst_key            VARCHAR(20),
+    cst_firstname      VARCHAR(50),
+    cst_lastname       VARCHAR(50),
+    cst_marital_status CHAR(10),
+    cst_gndr           CHAR(10),
+    cst_create_date    VARCHAR(20)
 );
 
 
@@ -124,15 +122,13 @@ CREATE TABLE crm_cust_info (
 DROP TABLE IF EXISTS crm_prd_info;
 
 CREATE TABLE crm_prd_info (
-
-    prd_id INT,
-    prd_key VARCHAR(50),
-    prd_nm VARCHAR(150),
-    prd_cost DECIMAL(10,2),
-    prd_line CHAR(1),
-    prd_start_dt DATE,
-    prd_end_dt DATE
-
+    prd_id         INT,
+    prd_key        VARCHAR(50),
+    prd_nm         VARCHAR(150),
+    prd_cost       DECIMAL(10,2),
+    prd_line       CHAR(20),
+    prd_start_dt   DATE,
+    prd_end_dt     DATE
 );
 
 
@@ -147,17 +143,15 @@ CREATE TABLE crm_prd_info (
 DROP TABLE IF EXISTS crm_sales_details;
 
 CREATE TABLE crm_sales_details (
-
-    sls_ord_num VARCHAR(20),
-    sls_prd_key VARCHAR(50),
-    sls_cust_id INT,
-    sls_order_dt DATE,
-    sls_ship_dt DATE,
-    sls_due_dt DATE,
-    sls_sales DECIMAL(10,2),
-    sls_quantity INT,
-    sls_price DECIMAL(10,2)
-
+    sls_ord_num     VARCHAR(20),
+    sls_prd_key     VARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    INT,
+    sls_ship_dt     INT,
+    sls_due_dt      INT,
+    sls_sales       DECIMAL(10,2),
+    sls_quantity    INT,
+    sls_price       DECIMAL(10,2)
 );
 
 
@@ -172,11 +166,9 @@ CREATE TABLE crm_sales_details (
 DROP TABLE IF EXISTS erp_cust_az12;
 
 CREATE TABLE erp_cust_az12 (
-
-    CID VARCHAR(30),
-    BDATE DATE,
-    GEN VARCHAR(20)
-
+    cid     VARCHAR(30),
+    bdate   DATE,
+    gen     VARCHAR(20)
 );
 
 
@@ -191,10 +183,8 @@ CREATE TABLE erp_cust_az12 (
 DROP TABLE IF EXISTS erp_loc_a101;
 
 CREATE TABLE erp_loc_a101 (
-
-    CID VARCHAR(30),
-    CNTRY VARCHAR(50)
-
+    cid     VARCHAR(30),
+    cntry   VARCHAR(50)
 );
 
 
@@ -209,12 +199,10 @@ CREATE TABLE erp_loc_a101 (
 DROP TABLE IF EXISTS erp_cat_g1v2;
 
 CREATE TABLE erp_cat_g1v2 (
-
-    ID VARCHAR(20),
-    CAT VARCHAR(100),
-    SUBCAT VARCHAR(100),
-    MAINTENANCE VARCHAR(10)
-
+    id            VARCHAR(20),
+    cat           VARCHAR(100),
+    subcat        VARCHAR(100),
+    maintenance   VARCHAR(10)
 );
 
 
